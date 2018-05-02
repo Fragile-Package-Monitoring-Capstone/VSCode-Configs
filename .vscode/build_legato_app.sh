5dd752c48c7cafcee61c22b63d8c872713846ade
Lukas Vann Struth <lvannstruth@gmail.com>
2018-05-02 14:10:26 -0700

    Renamed build task and created upload task.

--- .vscode/build_legato_app.sh
+++ .vscode/build_legato_app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# This is a shell script that is used to correctly compile
-LEGATO_ROOT="example" #Change This For Your System - This is where the legato directory is installed($LEGATO_ROOT"
+# This is a shell script that is used to correctly compile your legato application from within vscode's task. 
+LEGATO_ROOT="example" #Change This For Your System - This is where the legato directory is installed($LEGATO_ROOT")
 ADEF_FILE="example"#Change This For Your System - This is the adef file that should be sitting in the root of your legato app and is used by the build system in order to compile 
 pushd . 
 cd $LEGATO_ROOT || exit
